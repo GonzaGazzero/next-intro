1acbb798a5fddbbd8697f29d333d0a451b14846a
Gonzalo Gazzero <gonzagazzero2017@gmail.com>
2024-05-27 18:58:57 -0300

    Link y navigating

--- src/app/(marketing)/page.jsx
+++ src/app/(marketing)/page.jsx
@@ -1,9 +1,12 @@
 import TitlePage from '@/components/TitlePage'
+import Link from 'next/link'
 import React from 'react'
 
 const Home = () => {
   return <>
     <TitlePage title='Home' />
+    {/* <a href="/blog">Blog</a> ineficiente*/} 
+    <Link href='/blog'>Blog</Link> {/* hace navegacion desde el cliente evitando la peticion al servidor */}
   </>
 }
 
